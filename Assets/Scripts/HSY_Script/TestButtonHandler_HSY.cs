7924d01c4540c0510e7e68d4b92dc714ecfd54da
KimYC1223 <kau_esc@naver.com>
2021-01-13 17:00:11 +0900

    Update Test Scene

---
+++ Assets/Scripts/HSY_Script/TestButtonHandler_HSY.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButtonHandler_HSY : MonoBehaviour
+{
+    public CalendarWaterLogHandler LogHandler;
+    
+    public void ButtonClick() {
+        Debug.Log("buttonClick");
+    }
+}
---
+++ Assets/Scripts/KDH_Script/TestButtonHandler_KDH.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestButtonHandler_KDH : MonoBehaviour
+{
+    public InputField input;
+    public PeeScriptHandler handle;
+
+    public void ButtonClick() {
+        Debug.Log("버튼이 눌렸음");
+
+    }
+}
