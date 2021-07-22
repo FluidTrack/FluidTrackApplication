b98c1519a0a6eb1ae38c0234ee9b11452cdac921
KimYC1223 <kau_esc@naver.com>
2021-07-22 16:54:26 +0900

    update Scenes

---
+++ Assets/Scripts/CanvasHandler/NewHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeHandler.DateTimeStamp currentTime = new TimeHandler.DateTimeStamp("2021-07-22");
+        TimeHandler.DateTimeStamp targetTime = new TimeHandler.DateTimeStamp("2021-07-23");
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
