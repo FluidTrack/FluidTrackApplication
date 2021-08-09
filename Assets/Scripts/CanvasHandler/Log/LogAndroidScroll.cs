cb80c8e32155d1070c581bfa3f5d2886a78e3a60
sanghyuplee20 <slee548@jhu.edu>
2021-08-09 19:08:48 +0900

    Added Log Side Scrolling

---
+++ Assets/Scripts/CanvasHandler/Log/LogAndroidScroll.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogAndroidScroll : MonoBehaviour
+{
+    public int counter; 
+    public bool isRight; 
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //Initiate Variable
+        counter = 0;
+        isRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //To check if the right canvas are on
+        if (!TotalManager.instance.OtherCanvas[(int)TotalManager.CANVAS.FLOWER].activeSelf &&
+            TotalManager.instance.OtherCanvas[(int)TotalManager.CANVAS.LOG].activeSelf && Input.touchCount == 1) {
+            //Threshold 5
+            if (Input.GetTouch(0).deltaPosition.x < -5)
+            {
+                if (isRight)
+                {
+                    counter++;
+                } else
+                {
+                    isRight = true;
+                    counter = 0;
+                }
+            } else if (Input.GetTouch(0).deltaPosition.x > 5)
+            {
+                if (!isRight)
+                {
+                    counter++;
+                } else
+                {
+                    isRight = false;
+                    counter = 0;
+                }
+            }
+
+            //Move if counter hits 5
+            if (counter == 5)
+            {
+                if (isRight)
+                {
+                    LogCanvasHandler.Instance.TimeRightButtonClick();
+                }
+                else
+                {
+                    LogCanvasHandler.Instance.TimeLeftButtonClick();
+                }
+                counter = 0;
+            }
+        }
+    }
+}
